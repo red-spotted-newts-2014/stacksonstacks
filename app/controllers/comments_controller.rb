825df68ab57b3c53463b0001acdf6e41824d052a
Dylan Richards <dylanerichards@gmail.com>
2015-02-11 01:45:22 -0500

    Apply proper indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,56 +1,56 @@
 class CommentsController < ApplicationController
-	def index
-		# @commentable = find_commentable
-		# @comments = @commentable.comments
-	end
+  def index
+    # @commentable = find_commentable
+    # @comments = @commentable.comments
+  end
 
-	def show
-		@comment = Comment.find(params[:id])
-	end
+  def show
+    @comment = Comment.find(params[:id])
+  end
 
-	def new
-		@comment = Comment.new
-	end
+  def new
+    @comment = Comment.new
+  end
 
-	def create
-		@commentable = find_commentable
-		@comment = @commentable.comments.build(comment_params)
-		if @comment.save
-		  flash[:notice] = "Successfully created comment."
-		  redirect_to root_path
-		else
-		  render :action => 'new'
-		end
-	end
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(comment_params)
+    if @comment.save
+      flash[:notice] = "Successfully created comment."
+      redirect_to root_path
+    else
+      render :action => 'new'
+    end
+  end
 
-	def edit
-		@comment = Comment.find(params[:id])
-	end
+  def edit
+    @comment = Comment.find(params[:id])
+  end
 
-	def destroy
-		@comment = Comment.find(params[:id])
-		@comment.destroy
-		redirect_to comments_path 
-	end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to comments_path 
+  end
 
-	private
+  private
 
-	def comment_params
-		params.require(:comment).permit(:body)
-	end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
-	def find_commentable
-		puts params
-		puts "before loops"
-	  params["comment"].each do |name, value|
-	    if name =~ /(.+)_id$/
-	    	p $1
-	    	p $1.classify.constantize
-	    	p value.to_i
-	      return $1.classify.constantize.find(value.to_i)
-	    end
-	  end
-	  puts "after loop"
-	  nil
-	end
+  def find_commentable
+    puts params
+    puts "before loops"
+    params["comment"].each do |name, value|
+      if name =~ /(.+)_id$/
+        p $1
+        p $1.classify.constantize
+        p value.to_i
+        return $1.classify.constantize.find(value.to_i)
+      end
+    end
+    puts "after loop"
+    nil
+  end
 end
