e72b992354af4cd277dff0801c52947b46c4ba67
Dylan Richards <dylanerichards@gmail.com>
2015-02-11 01:35:25 -0500

    Clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,8 @@
 class UsersController < ApplicationController
   def index
-
   end
- 
 
-
-  # Devise does not generate a show view for users. Overwrote
-  # the UsersController and implemented our own for user profiles.
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
-
-
 end
